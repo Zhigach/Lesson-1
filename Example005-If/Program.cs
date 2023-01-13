c0f11cf037d57830d8c0d6ce61019f173dde0f32
Alex <sashazhig@gmail.com>
2023-01-13 08:04:47 +0300

    Forgotten files

---
+++ Example005-If/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter your name");
+string name = Console.ReadLine();
+
+if (name.ToLower() == "masha"){
+    Console.WriteLine("Hurray, it's Masha!!!!");
+} else {
+    Console.WriteLine("hi," + name);
+}
